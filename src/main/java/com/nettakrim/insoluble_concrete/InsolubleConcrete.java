21c3ee32f1a13e16dad3293c78569fb8651426b9
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2024-01-02 14:54:16 +0000

    initial commit, theoretical functionality for cancelling concrete hardening

---
+++ src/main/java/com/nettakrim/insoluble_concrete/InsolubleConcrete.java
@@ -0,0 +1,15 @@
+package com.nettakrim.insoluble_concrete;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InsolubleConcrete implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("insoluble_concrete");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/nettakrim/insoluble_concrete/mixin/ConcretePowderBlockMixin.java
@@ -0,0 +1,14 @@
+package com.nettakrim.insoluble_concrete.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(net.minecraft.block.ConcretePowderBlock.class)
+public class ConcretePowderBlockMixin {
+	@ModifyReturnValue(at = @At("RETURN"), method = "shouldHarden")
+	private static boolean init(boolean original) {
+		return false;
+	}
+}
